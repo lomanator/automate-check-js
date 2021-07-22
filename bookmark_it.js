ec93db0c3b7b7ca55df49443d3b4f92fb74b28ed
lomanator <87831054+lomanator@users.noreply.github.com>
2021-07-22 16:38:45 -0300

    Update bookmark_it.js

--- bookmark_it.js
+++ bookmark_it.js
@@ -1,1 +1,1 @@
-javascript: var script = document.createElement("script"); script.type = "text/javascript"; script.src = "https://arapozojr.github.io/automate-check-js/app.js"; document.head.appendChild(script); eval(script);
+javascript: var script = document.createElement("script"); script.type = "text/javascript"; script.src = "https://lomanator.github.io/automate-check-js/app.js"; document.head.appendChild(script); eval(script);
