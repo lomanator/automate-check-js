d4b11f7f88dd536d00120fcf08b809ab6d5cfefb
Adilson Rapozo Junior <adilson.rapozo@automatos.com>
2021-07-22 15:12:26 -0300

    Adding index.html to test it out

---
+++ bookmark_it.js
@@ -0,0 +1,1 @@
+javascript: var script = document.createElement("script"); script.type = "text/javascript"; script.src = "https://arapozojr.github.io/automate-check-js/app.js"; document.head.appendChild(script); eval(script);
