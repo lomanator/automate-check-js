3ef8294f03cf8908e2ac98597790e946aa1dbb6d
Adilson Rapozo Junior <adilson.rapozo@automatos.com>
2021-08-30 20:55:41 -0300

    Procurando por um filtro em um arquivo enviado

--- relatorio.js
+++ relatorio.js
@@ -5,44 +5,64 @@ var table = document.getElementById(TABLE_ID);
 var headerHTML = table.getElementsByTagName('thead')[0].rows[0].cells;
 var rowsHTML = table.getElementsByTagName('tbody')[0].rows;
 
-var headerArray = []
-for (var i = 0; i < headerHTML.length; i++) {
-  headerArray.push(headerHTML[i].innerText);
-}
+alert('Selecione arquivo com os resultados desejados.');
 
-var rowsArray = [ headerArray ];
-var cellArray = []
+var myFile = document.createElement('input');
+myFile.type = 'file';
+myFile.click();
 
-var i = 0;
-var firstValue = ''
-while (i < rowsHTML.length - 1) {
-  firstValue = rowsHTML[i].cells[0].innerText;
-  if (!isNaN(firstValue)) { //Linha de dados
-    resultText = rowsHTML[i+1].cells[0].innerText;
-    if (resultText.trim().toLowerCase() === 'REGISTRO AR - ASSINA'.trim().toLowerCase()){
-      cellArray = [];
-      for (j = 0; j< rowsHTML[i].cells.length; j++)
-        cellArray.push(rowsHTML[i].cells[j].innerText);
-      cellArray.push(resultText);
-      rowsArray.push(cellArray);
+myFile.onchange = function(){
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+
+    var searchingTerms = this.result.split('\n');
+    searchingTerms = searchingTerms.map(s => s.replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g, ' ').trim().toLowerCase());
+    searchingTerms = searchingTerms.filter(s => s ? true : false);
+
+    var headerArray = []
+    for (var i = 0; i < headerHTML.length; i++) {
+      headerArray.push(headerHTML[i].innerText);
     }
-  }
-  i++;
-}
 
-var dateObj = new Date();
-var month = dateObj.getUTCMonth() + 1; //months from 1-12
-var day = dateObj.getUTCDate();
-var year = dateObj.getUTCFullYear();
+    var rowsArray = [ headerArray ];
+    var cellArray = []
 
-newdate = year + "-" + month + "-" + day;
+    var i = 0;
+    var firstValue = ''
+    while (i < rowsHTML.length - 1) {
+      firstValue = rowsHTML[i].cells[0].innerText;
+      if (!isNaN(firstValue)) { //Linha de dados
+        resultText = rowsHTML[i+1].cells[0].innerText;
+        if (searchingTerms.indexOf(resultText.replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g, ' ').trim().toLowerCase()) > -1){
+          cellArray = [];
+          for (j = 0; j< rowsHTML[i].cells.length; j++)
+            cellArray.push(rowsHTML[i].cells[j].innerText);
+          cellArray.push(resultText);
+          rowsArray.push(cellArray);
+        }
+      }
+      i++;
+    }
 
-var csvContent = "data:text/csv;charset=utf-8,"
-  + rowsArray.map(e => e.join(";")).join("\n");
-var encodedUri = encodeURI(csvContent);
-var link = document.createElement("a");
-link.setAttribute("href", encodedUri);
-link.setAttribute("download", "NDS_bloqueadas_" + newdate + ".csv");
-document.body.appendChild(link);
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
 
-link.click();
+    newdate = year + "-" + month + "-" + day;
+
+    var csvContent = "data:text/csv;charset=utf-8,"
+      + rowsArray.map(e => e.join(";")).join("\n");
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "NDS_bloqueadas_" + newdate + ".csv");
+    document.body.appendChild(link);
+
+    alert('Relatório gerado com ' + rowsArray.length + ' items encontrados. Salvando...')
+    link.click();
+  };
+  reader.readAsText(file);
+};
